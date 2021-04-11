a8cab743ffd012de600e52cfd640c7f6fa1c9ad1
장상옥 <jso46034@naver.com>
2021-04-11 16:35:59 +0900

    Interface를 이용하여 java코드를 수정하지 않고 config 파일만 수정하여 원하는 값을 얻어냄.

---
+++ springEx_04/src/main/java/com/java/springEx_04/Pencil.java
@@ -0,0 +1,5 @@
+package com.java.springEx_04;
+
+public interface Pencil {
+	public void use();
+}
---
+++ springEx_04/src/main/java/com/java/springEx_04/Pencil6B.java
@@ -0,0 +1,10 @@
+package com.java.springEx_04;
+
+public class Pencil6B implements Pencil {
+
+	@Override
+	public void use() {
+		System.out.println("6B 굵기로 씁니다.");
+	}
+
+}
