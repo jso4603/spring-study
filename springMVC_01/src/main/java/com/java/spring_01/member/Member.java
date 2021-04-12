cbd1996564e50b2eb1586272d48c57cf68a84bd8
장상옥 <jso46034@naver.com>
2021-04-12 20:50:21 +0900

    HttpServletRequest, @RequestParm, 데이터(커맨드) 객체 study

---
+++ springMVC_01/src/main/java/com/java/spring_01/member/Member.java
@@ -0,0 +1,20 @@
+package com.java.spring_01.member;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+
+	public String name;
+
+	public String id;
+
+	public String pw;
+
+	public String email;
+
+}
